d5a7bfdb722c604b94de515402ed323d08553b68
Panagiotis Koutsaftikis <p.kouts153@gmail.com>
2013-09-02 19:54:14 +0300

    half complete home screen

--- gen/com/SR/smartreceipt/R.java
+++ gen/com/SR/smartreceipt/R.java
@@ -39,6 +39,20 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int heading=0x7f050002;
+        public static final int link1=0x7f050003;
+        public static final int link2=0x7f050006;
+        public static final int link3=0x7f050007;
+        public static final int link4=0x7f05000a;
+        public static final int link5=0x7f05000c;
+        public static final int link6=0x7f05000e;
+        public static final int link7=0x7f050010;
+        public static final int page1=0x7f050004;
+        public static final int page2=0x7f050005;
+        public static final int page3=0x7f050008;
+        public static final int page4=0x7f050009;
+        public static final int page5=0x7f05000b;
+        public static final int page6=0x7f05000d;
+        public static final int page7=0x7f05000f;
     }
     public static final class style {
         /** 
