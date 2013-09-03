e8e021c3e8de4379972a0aed19ece4f37d15b250
Panagiotis Koutsaftikis <p.kouts153@gmail.com>
2013-09-03 14:47:11 +0300

    added link methods

--- src/com/SR/smartreceipt/MainActivity.java
+++ src/com/SR/smartreceipt/MainActivity.java
@@ -3,6 +3,7 @@ package com.SR.smartreceipt;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +18,41 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	/** Called when the user clicks the Save link */
+	public void goToSave(View view) {
+	    // Do something in response to button
+	}
+	
+	/** Called when the user clicks the Budget link */
+	public void goToBudget(View view) {
+	    // Do something in response to button
+	}
+	
+	/** Called when the user clicks the Search link */
+	public void goToSearch(View view) {
+	    // Do something in response to button
+	}
+	
+	/** Called when the user clicks the Offers link */
+	public void goToOffers(View view) {
+	    // Do something in response to button
+	}
+	
+	/** Called when the user clicks the Family link */
+	public void goToFamily(View view) {
+	    // Do something in response to button
+	}
+	
+	/** Called when the user clicks the List link */
+	public void goToList(View view) {
+	    // Do something in response to button
+	}
+	
+	/** Called when the user clicks the Stores link */
+	public void goToStores(View view) {
+	    // Do something in response to button
 	}
 
 }
