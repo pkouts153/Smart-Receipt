06dd798aaa7eed0f8e65de1ca5578da08aab923b
Panagiotis Koutsaftikis <p.kouts153@gmail.com>
2013-09-03 19:56:25 +0300

    completed main activity layout

--- gen/com/SR/smartreceipt/R.java
+++ gen/com/SR/smartreceipt/R.java
@@ -22,9 +22,17 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int drawable_horizontal_margin=0x7f040002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int budget=0x7f020000;
+        public static final int family=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int list=0x7f020003;
+        public static final int map=0x7f020004;
+        public static final int offers=0x7f020005;
+        public static final int save=0x7f020006;
+        public static final int search=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f090000;
